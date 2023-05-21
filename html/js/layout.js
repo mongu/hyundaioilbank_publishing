ff4b98a7f3b4816d45b52302d46f1e18c4f9c1cd
정유진 <mjay0906@gmail.com>
2023-05-22 00:59:48 +0900

    로그인 수정, 팝업 추가, 대시보드 수정

--- html/js/layout.js
+++ html/js/layout.js
@@ -110,7 +110,7 @@ $(".toggle-password").click(function () {
 
 //datepicker calendar
 $( function() {
-	$( "#datepicker" ).datepicker({
+	$( "#datepicker, #datepicker2, #datepicker3, #datepicker4, #datepicker5" ).datepicker({
 		dateFormat: "yy-mm-dd",
         dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
         monthNames: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
@@ -142,14 +142,45 @@ $(window).resize(function () {
 })
 
 
+// timepicker
+var options1 = {
+    now: "00:00", //hh:mm 24 hour format only, defaults to current time
+    twentyFour: true,  //Display 24 hour format, defaults to false
+    upArrow: 'wickedpicker__controls__control-up',  //The up arrow class selector to use, for custom CSS
+    downArrow: 'wickedpicker__controls__control-down', //The down arrow class selector to use, for custom CSS
+    close: 'wickedpicker__close', //The close class selector to use, for custom CSS
+    hoverState: 'hover-state', //The hover state class to use, for custom CSS
+    title: '', //The Wickedpicker's title,
+    showSeconds: false, //Whether or not to show seconds,
+    timeSeparator: ' : ', // The string to put in between hours and minutes (and seconds)
+    secondsInterval: 1, //Change interval for seconds, defaults to 1,
+    minutesInterval: 10, //Change interval for minutes, defaults to 1
+    beforeShow: null, //A function to be called before the Wickedpicker is shown
+    afterShow: null, //A function to be called after the Wickedpicker is closed/hidden
+    show: null, //A function to be called when the Wickedpicker is shown
+    clearable: false, //Make the picker's input clearable (has clickable "x")
+};
 
+var options2 = {
+    now: "24:00", //hh:mm 24 hour format only, defaults to current time
+    twentyFour: true,  //Display 24 hour format, defaults to false
+    upArrow: 'wickedpicker__controls__control-up',  //The up arrow class selector to use, for custom CSS
+    downArrow: 'wickedpicker__controls__control-down', //The down arrow class selector to use, for custom CSS
+    close: 'wickedpicker__close', //The close class selector to use, for custom CSS
+    hoverState: 'hover-state', //The hover state class to use, for custom CSS
+    title: '', //The Wickedpicker's title,
+    showSeconds: false, //Whether or not to show seconds,
+    timeSeparator: ' : ', // The string to put in between hours and minutes (and seconds)
+    secondsInterval: 1, //Change interval for seconds, defaults to 1,
+    minutesInterval: 10, //Change interval for minutes, defaults to 1
+    beforeShow: null, //A function to be called before the Wickedpicker is shown
+    afterShow: null, //A function to be called after the Wickedpicker is closed/hidden
+    show: null, //A function to be called when the Wickedpicker is shown
+    clearable: false, //Make the picker's input clearable (has clickable "x")
+};
 
-
-
-
-
-
-
+$('.timepicker1').wickedpicker(options1);
+$('.timepicker2').wickedpicker(options2);
 
 
 
